8c466a81febd18a324dbe23d0bf4153df1f6cbd6
hiroingonda-dev <hiroingonda@gmail.com>
2025-12-03 23:44:49 +0530

    Update routes.js
    fixed a bug where the previous get method overrides the required get call

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,9 +4,6 @@ import { getCandyList } from "../controllers/controllers.js";
 const router = express.Router();
 
 // get methods
-router.get("/", (req, res) => {
-  res.send("Website is live really ");
-});
 
 router.get("/",getCandyList);
 
